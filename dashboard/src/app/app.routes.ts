971305c887c9aa35070130952618e7722944e377
PauloHGrande <pauloav2013@gmail.com>
2025-02-07 10:43:27 -0300

    Finalização do Layout

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './shared/components/layout/layout.component';
+import { DashboardComponent } from './business/dashboard/dashboard.component';
+import { TablesComponent } from './business/tables/tables.component';
+import { ProfileComponent } from './business/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: 'profile',
+                component: ProfileComponent
+            },
+            {
+                path: 'tables',
+                component: TablesComponent
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+    }
+
+];
